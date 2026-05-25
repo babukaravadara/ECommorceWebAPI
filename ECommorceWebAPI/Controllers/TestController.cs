b2232739c3a8ff96eaa5622f6bcacf18fac3f63e
babu <karavdrababu@gmail.com>
2026-05-25 18:41:10 +0530

    fixed issue

---
+++ ECommorceWebAPI/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommorceWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
